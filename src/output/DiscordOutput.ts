f02a2d8523acee5571b44a43a73eca2a16fc0526
Aaron Static <aaronstatic@gmail.com>
2023-07-19 02:25:09 +1000

    Handle discord output when input is not discord

--- src/output/DiscordOutput.ts
+++ src/output/DiscordOutput.ts
@@ -5,6 +5,15 @@ export default class DiscordOutput extends BaseOutput {
     async send(message: string, originator: any) {
         const discord = this.getEnv('discord') as DiscordEnvironment;
         if (!discord) return;
+        if (!originator || !originator.channel) {
+            if (this.config && this.config.channel && discord.client) {
+                const channel = await discord.client.channels.fetch(this.config.channel);
+                originator = { channel: channel };
+            } else {
+                this.log("No channel specified for DiscordOutput");
+                return;
+            }
+        }
         originator.channel.send(message);
     }
 }
