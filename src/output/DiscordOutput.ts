4148ac5e8c1605504a9472bdef712825419c033c
Aaron Static <aaronstatic@gmail.com>
2023-07-19 02:33:42 +1000

    Discord output error handling

--- src/output/DiscordOutput.ts
+++ src/output/DiscordOutput.ts
@@ -8,9 +8,13 @@ export default class DiscordOutput extends BaseOutput {
         if (!originator || !originator.channel) {
             if (this.config && this.config.channel && discord.client) {
                 const channel = await discord.client.channels.fetch(this.config.channel);
+                if (!channel) {
+                    this.log("WARNING: Could not find channel with ID " + this.config.channel);
+                    return;
+                }
                 originator = { channel: channel };
             } else {
-                this.log("No channel specified for DiscordOutput");
+                this.log("WARNING: Must specify channel ID in config when input is not also Discord");
                 return;
             }
         }
